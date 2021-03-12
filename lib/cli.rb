fed1ecf3d6ff0bc222343c1fdccc98b0bcff8cd3
Carlos Fernandez <jcfernan@live.com>
2021-03-12 15:03:08 -0700

    Built out cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,30 @@
 class Cli
-    
+
+    attr_reader :user
+
+    def prompt
+        TTY::Prompt.new
+    end
+
+    # def initialize user=nil
+    #     @user = user
+    # end
+
+    def welcome
+        system('clear')
+        puts "WELCOME TO SHOW SELECTOR THE #1 SPOT FOR SHOWS!!!"
+        ask = prompt.yes?("Would you like to select some shows?")
+        if ask
+            list_shows
+        else
+            puts "Too bad"
+            welcome
+        end
+    end
+
+    def list_shows
+        puts Show.all.pluck(:title)
+    end
+
+
 end
